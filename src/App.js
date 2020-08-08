de3d56bc62c265b829d7e5af3b23169df1983121
Natalia López <natalialopezyeste@gmail.com>
2020-08-08 10:43:13 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "bootstrap/dist/css/bootstrap.min.css";
+import './App.css';
+
+function App() {
+  return (
+    <div>
+     <h1>Kokomo</h1>
+    </div>
+  );
+}
+
+export default App;
