af9e747d385ebf8508014bd9c8b60e4251447055
Natalia López <natalialopezyeste@gmail.com>
2020-08-25 21:34:37 +0200

    Cambios en App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import { Switch, Route } from 'react-router-dom'
 
 function App() {
   return (
     <div>
      <h1>Kokomo</h1>
+     <Switch>
+            <Route exact path="/">
+              <h1>Esta es la homepage</h1>
+            </Route>
+
+            {/* 
+              CON LA KEY SE VUELVE A CARGAR TODO CADA VEZ QUE SE MODIFICA ALGO
+            <Route exact path="/projects" render={(props) => <ProjectList {...props} user={this.state.loggedInUser} key={this.state.loggedInUser}/>} /> */}
+
+            {/* <Route path="/projects/:id" render={(props) => <ProjectDetails {...props} />} /> */}
+            <Route exact path="/fileupload">
+              {/* componente */}
+            </Route>
+            <Route path="/signup" render={(props) => <Signup {...props} callback={this.getTheUser} />} />
+            <Route path="/login" render={(props) => <Login {...props} callback={this.getTheUser} />} />
+            <Route path="/logout" render={(props) => <Logout {...props} callback={this.getTheUser} />} />
+            <ProtectedRoute
+              path="/profile"
+              user={this.state.loggedInUser}
+              component={Profile}
+              />
+        </Switch>
     </div>
   );
 }
