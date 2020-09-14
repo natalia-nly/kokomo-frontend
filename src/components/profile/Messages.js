22e2fb82669fa23fdf2dbd5ada427fd11c8e79f8
Claudi Sanchez <claudi.sanchezserrano@gmail.com>
2020-09-14 18:46:59 +0200

    delete message

--- src/components/profile/Messages.js
+++ src/components/profile/Messages.js
@@ -1,5 +1,8 @@
 import React, { useState, useEffect } from "react";
 import AuthService from "../../services/auth/auth-service";
+import ProfileService from "../../services/profile/profile-service"
+
+const profileService = new ProfileService();
 const service = new AuthService();
 
 let initialState = {
@@ -23,7 +26,18 @@ const Messages = (props) => {
     });
   }, []);
 
-  
+ 
+
+  const deleteMessage = (e) => {
+    console.log(e.target.messageId.value)
+      profileService.deleteMessage(e.target.messageId.value)
+      .then((response) => {
+        console.log(response);
+      });
+    
+  };
+
+
 
   let allMessages = <p>No tienes Notificaciones pendientes</p>;
   if (state.messages.length) {
@@ -44,6 +58,12 @@ const Messages = (props) => {
           </h5>
           <h4>Tema: {message.topic}</h4>
           <p>{message.message}</p>{" "}
+            <form onSubmit={deleteMessage}  className="btn-kokomo btn-kokomo-danger mr-3 mb-4">
+            <input type="hidden" value={message._id} name="messageId"/>    
+              <button className="link-danger">
+                <i className="far fa-trash-alt"></i>
+              </button>
+            </form>
         </div>
     ));
   }
